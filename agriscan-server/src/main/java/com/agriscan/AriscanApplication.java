3526aac2854e29fff86e0749796b4711073eca40
aloknaik01 <aloknaik016261@gmail.com>
2026-03-24 22:59:20 +0530

    initialize agriscan repo

---
+++ agriscan-server/src/main/java/com/agriscan/AriscanApplication.java
@@ -0,0 +1,13 @@
+package com.agriscan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AriscanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AriscanApplication.class, args);
+	}
+
+}
---
+++ agriscan-server/src/test/java/com/agriscan/AriscanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agriscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AriscanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
